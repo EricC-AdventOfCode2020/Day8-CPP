5a8ec5c9ae3f4313209715dac5cbe9e520d52870
Eric Comanescu <ericcom@outlook.com>
2023-10-24 09:43:02 -0400

    Added initial files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "utility.h"
+
+using namespace std;
+
+int part_1(vector<string> data);
+int part_2(vector<string> data);
+
+int main(int argc, char const *argv[])
+{
+    vector<string> data = read_input("./input");
+    return 0;
+}
