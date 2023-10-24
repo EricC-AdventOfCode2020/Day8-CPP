c91e3bfa2df3c0362b61f17f0a53c7db5652b544
Eric Comanescu <ericcom@outlook.com>
2023-10-24 13:10:35 -0400

    penice

--- main.cpp
+++ main.cpp
@@ -2,18 +2,45 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <functional>
 #include "utility.h"
 
 using namespace std;
+using func = function<void(int*, vector<string>)>;
 
-int part_1(vector<string> data);
-int part_2(vector<string> data);
+using func = function<void(int*, vector<string>, int*)>;
+
+int part_1(vector<string> data, func);
+int part_2(vector<string> data, func);
+
+void nop(int *accumulator, vector<string>, int *instruction);
+void acc(int *accumulator, vector<string>, int *instruction);
+void jmp(int *accumulator, vector<string>, int *instruction);
 
 int main(int argc, char const *argv[])
 {
     vector<string> data = read_input("./input");
 
-    
+    map<string, func> instructions
+    {
+        {"nop", nop},
+        {"acc", acc},
+        {"jmp", jmp}
+    };
 
+    int accumulator = 0;
+
+    cout << "Part 1: " << part_1(data, instructions) << endl;
     return 0;
+}
+
+int part_1(vector<string> data, map< string, func> instructions)
+{
+    int accumulator = 0;
+
+    vector<int> readInstructions;
+
+    for (int instruction = 0; instruction < data.size(); instruction++)
+
+    return accumulator;
 }
